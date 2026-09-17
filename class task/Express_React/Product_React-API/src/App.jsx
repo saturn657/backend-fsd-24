3ef15872a1beecdc500bb1d49ac7b4731462bcf2
Mohit  Suri <saturn657@proton.me>
2026-09-17 21:54:50 +0530

    class tasks

---
+++ class task/Express_React/Product_React-API/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+const App = () => {
+  const[products,setProducts]=useState([]);
+  const[name,setName]=useState("");
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
